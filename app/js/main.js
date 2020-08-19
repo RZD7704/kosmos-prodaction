0816ad56a3713f250b7cb6f00e99782fb202adf3
Artem <artemius2014poltava@gmail.com>
2020-08-19 17:15:46 +0300

    Add web services

--- app/js/main.js
+++ app/js/main.js
@@ -127,6 +127,44 @@ var swiper4 = new Swiper('.slider__instagram', {
   }
 });
 
+var swiper5 = new Swiper('.slider__web-portfolio', {
+  slidesPerView: 1,
+  spaceBetween: 90,
+  navigation: {
+    nextEl: '.section-header__btn-slider_next-web',
+    prevEl: '.section-header__btn-slider_prev-web',
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20
+      },
+    400: {
+      slidesPerView: 2,
+      spaceBetween: 20
+      },
+    // when window width is >= 480px
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 40
+    },
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 40
+    },
+    2240: {
+      slidesPerView: 5,
+      spaceBetween: 60
+    }
+  }
+});
+
 
 
 
