d8896f507a18e52c1d349cbbd53a10206c2b5c62
Artem <artemius2014poltava@gmail.com>
2020-08-21 14:23:32 +0300

    Add new pages

--- app/js/main.js
+++ app/js/main.js
@@ -165,6 +165,43 @@ var swiper5 = new Swiper('.slider__web-portfolio', {
   }
 });
 
+var swiper6 = new Swiper('.slider__single-service', {
+  slidesPerView: 1,
+  spaceBetween: 90,
+  navigation: {
+    nextEl: '.section-header__btn-slider_next-single-service',
+    prevEl: '.section-header__btn-slider_prev-single-service',
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20
+      },
+    400: {
+      slidesPerView: 2,
+      spaceBetween: 20
+      },
+    // when window width is >= 480px
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 40
+    },
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 40
+    },
+    2240: {
+      slidesPerView: 5,
+      spaceBetween: 60
+    }
+  }
+});
 
 
 
