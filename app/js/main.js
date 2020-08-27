052cea0ba7363538326a9aa6c6dbf15c25f6c898
Artem <artemius2014poltava@gmail.com>
2020-08-27 22:29:10 +0300

    Add last changes

--- app/js/main.js
+++ app/js/main.js
@@ -205,7 +205,7 @@ var swiper6 = new Swiper('.slider__single-service', {
 
 var swiper = new Swiper('.post-slider', {
   slidesPerView: 1,
-  spaceBetween: 30,
+  spaceBetween: 10,
   pagination: {
     el: '.post-slider-pagination',
     type: 'progressbar',
@@ -216,6 +216,66 @@ var swiper = new Swiper('.post-slider', {
   },
 });
 
+const time = 1000;
+const step = 10;
+
+function outNum(num, elem) {
+  let element = document.querySelector('#' + elem);
+  let number = 0;
+
+  let t = Math.round(time/(num/step));
+  let interval = setInterval(() => {
+    number = number + step;
+    if(number == num) {
+      clearInterval(interval);
+    }
+    element.innerHTML = number;
+  },
+  t);
+}
+
+
+
+var element = document.querySelector('#target');
+
+var Visible = function (target) {
+  // Все позиции элемента
+  var targetPosition = {
+      top: window.pageYOffset + target.getBoundingClientRect().top,
+      left: window.pageXOffset + target.getBoundingClientRect().left,
+      right: window.pageXOffset + target.getBoundingClientRect().right,
+      bottom: window.pageYOffset + target.getBoundingClientRect().bottom
+    },
+    // Получаем позиции окна
+    windowPosition = {
+      top: window.pageYOffset,
+      left: window.pageXOffset,
+      right: window.pageXOffset + document.documentElement.clientWidth,
+      bottom: window.pageYOffset + document.documentElement.clientHeight
+    };
+
+  if (targetPosition.bottom > windowPosition.top && // Если позиция нижней части элемента больше позиции верхней чайти окна, то элемент виден сверху
+    targetPosition.top < windowPosition.bottom && // Если позиция верхней части элемента меньше позиции нижней чайти окна, то элемент виден снизу
+    targetPosition.right > windowPosition.left && // Если позиция правой стороны элемента больше позиции левой части окна, то элемент виден слева
+    targetPosition.left < windowPosition.right) { // Если позиция левой стороны элемента меньше позиции правой чайти окна, то элемент виден справа
+    // Если элемент полностью видно, то запускаем следующий код
+    outNum(50, 'out-1');
+    outNum(500, 'out-2');
+    outNum(1000, 'out-3');
+    outNum(3000, 'out-4');
+  } else {
+    // Если элемент не видно, то запускаем этот код
+    console.clear();
+  };
+};
+
+// Запускаем функцию при прокрутке страницы
+window.addEventListener('scroll', function() {
+  Visible (element);
+});
+
+// А также запустим функцию сразу. А то вдруг, элемент изначально видно
+Visible (element);
 
 
 
